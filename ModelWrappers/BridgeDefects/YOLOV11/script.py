5f127159a218ab33127c76351c840d3317affbd3
KumarShivam1908 <shivam.pragati3299@gmail.com>
2025-02-04 23:33:39 +0530

    Final

--- ModelWrappers/BridgeDefects/YOLOV11/script.py
+++ ModelWrappers/BridgeDefects/YOLOV11/script.py
@@ -1,3 +1,11 @@
-from ModelWrappers.TrackDefects.Yolov11 import Yolov11
+from ModelWrappers.TrackDefects.Yolov11.yolov11 import YOLOv11Trainer
+trainer = YOLOv11Trainer(
+    data_yaml_path=r"", # Add path to data.yaml
+    batch_size=4, 
+    img_size=640  # Reduced image size
+)
+
+# Train and get results
+training_results = trainer.train()
 
 
